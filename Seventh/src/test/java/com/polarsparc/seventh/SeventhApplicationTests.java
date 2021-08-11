c5a40080210d702b7f63b9d8425707f3b14ea1c1
BhaskarS <bswamina@gmail.com>
2021-08-11 16:48:58 -0400

    Fixed issue with fourth.drl and added additional drl

---
+++ Seventh/src/test/java/com/polarsparc/seventh/SeventhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarsparc.seventh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeventhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
